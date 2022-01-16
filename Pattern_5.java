0d104444cb13eea9bd274e566964359bc88474ab
anubha193 <88902737+anubha193@users.noreply.github.com>
2022-01-16 09:53:37 -0500

    Create Pattern_5.java

---
+++ Pattern_5.java
@@ -0,0 +1,25 @@
+public class Pattern_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=2;j<=i;j++)
+			{
+				System.out.print("  ");
+			}
+			for(int k=5;k>=i;k--)
+			{
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
+/* * * * *
+   * * * *
+     * * *
+       * *
+         */
