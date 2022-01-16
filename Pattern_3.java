58f40410f53394b9ed1f39bcea7351f84519c942
anubha193 <88902737+anubha193@users.noreply.github.com>
2022-01-16 09:47:57 -0500

    Create Pattern_3.java

---
+++ Pattern_3.java
@@ -0,0 +1,21 @@
+public class Pattern_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=5;j>=i;j--)
+			{
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
+/* * * * *
+ * * * *
+ * * *
+ * *
+ */
