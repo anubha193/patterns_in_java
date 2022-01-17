1fbee2d7eb9720f97123157c417ffcba3da6c93e
anubha193 <88902737+anubha193@users.noreply.github.com>
2022-01-17 02:31:40 -0500

    Update Patt18.java

--- javaPatterns/Patt18.java
+++ javaPatterns/Patt18.java
@@ -4,7 +4,21 @@ public class Patt18 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method 6
-		for(
+		for(int i=1;i<=7;i++)
+		{
+			for(int j=1;j<=7;j++)
+			{
+				if(i==j||i+j==8)
+				{
+					System.out.print(" *");
+				}
+				else
+				{
+				System.out.print("  ");
+			}
+			}
+			System.out.println();
+		}
 
 	}
 
